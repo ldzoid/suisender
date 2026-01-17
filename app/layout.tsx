805ebe1d634092b34435307f2a552eb2d42760d9
ldzoid <lucijandrnasin1@gmail.com>
2026-01-17 11:31:32 +0100

    fix google icon problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,14 @@ export const metadata: Metadata = {
     template: `%s | ${APP_NAME}`,
   },
   description: APP_DESCRIPTION,
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "32x32" },
+      { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-icon.png", sizes: "180x180", type: "image/png" }],
+  },
   keywords: [
     "sui sender",
     "free sui multisender",
